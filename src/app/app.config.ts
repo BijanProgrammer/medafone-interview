2dc10dfaa835e4becf6c2814af11e8ccb15bc227
BijanProgrammer <bijaneisapour@gmail.com>
2024-05-29 14:05:47 +0330

    [ADD] toastr package

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
 import {ApplicationConfig} from '@angular/core';
+import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideRouter} from '@angular/router';
+
+import {provideToastr} from 'ngx-toastr';
 
 import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes)],
+    providers: [
+        provideRouter(routes),
+        provideAnimations(),
+        provideToastr({
+            tapToDismiss: true,
+            progressBar: true,
+            progressAnimation: 'decreasing',
+            timeOut: 2500,
+            extendedTimeOut: 1000,
+            disableTimeOut: false,
+            maxOpened: 5,
+            autoDismiss: true,
+            easeTime: 100,
+            positionClass: 'toast-bottom-right',
+        }),
+    ],
 };
