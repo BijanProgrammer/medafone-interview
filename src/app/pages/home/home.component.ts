709fa34d026b262598071770ae1866da035cb9cc
BijanProgrammer <bijaneisapour@gmail.com>
2024-05-29 12:23:38 +0330

    [ADD] task service [ADD] task styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {TaskService} from '../../services/task.service';
 
 @Component({
     selector: 'app-home',
@@ -7,4 +8,6 @@ import {Component} from '@angular/core';
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+    public constructor(public taskService: TaskService) {}
+}
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+
+import {TaskModel} from '../models/task.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class TaskService {
+    public constructor() {}
+
+    private _tasks: TaskModel[] = [
+        {
+            id: self.crypto.randomUUID(),
+            title: 'Sample Task',
+            description: 'This is a sample task. You can edit it or remove it completely.',
+        },
+    ];
+
+    public get tasks(): TaskModel[] {
+        return [...this._tasks];
+    }
+
+    public addTask(task: TaskModel): void {
+        // [BIJAN] TODO: Add validation here or before this method is called.
+        this._tasks.push(task);
+    }
+}
