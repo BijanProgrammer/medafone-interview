ba5cc460a091bc5b57c3c0d4f90c761b9992e629
BijanProgrammer <bijaneisapour@gmail.com>
2024-05-29 12:06:14 +0330

    [ADD] header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+import {HomeComponent} from './pages/home/home.component';
+import {TaskComponent} from './pages/task/task.component';
+
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', component: HomeComponent},
+    {path: 'task', component: TaskComponent},
+];
